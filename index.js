66c4f507f48f8510f2b7e5282d1b24f5f653d19c
Raudy <raudycode@gmail.com>
2025-05-20 19:32:53 -0400

    agregue variables parra los puntajes

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// registra las puntuaciones de los jugadores
+let humanScore = 0
+    computerScore = 0
+
+
 function getComputerChice(){
     // la computadora selecionara un numero aleatorio
     let numberOption = Math.floor((Math.random() * 3) + 1)
