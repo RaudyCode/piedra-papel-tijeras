7713c765c040c816497417b7cae2344ff918ba6a
Raudy <raudycode@gmail.com>
2025-05-20 19:27:45 -0400

    Agregue la funcion que permite a la pc seleccionar una opcion

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+function getComputerChice(){
+    // la computadora selecionara un numero aleatorio
+    let numberOption = Math.floor((Math.random() * 3) + 1)
+    
+    // la computadora retornara ese numero
 
-
+    return numberOption
+}
